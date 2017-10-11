c02bd7ffb8383629c738c8feea1a153d774152c4
Mimansa <mimansabaghel@gmail.com>
2017-10-11 14:49:57 +0530

    second commit

---
+++ bubble.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+#include<conio.h>
+main()
+{
+int array[10000],n,c,d,swap; 
+printf("number of elements??\n");
+scanf("%d",&n);
+printf("Enter %d integers\n", n);
+for (c=0;c<n;c++)
+scanf("%d",&array[c]);
+for (c=0;c<(n-1);c++)
+{
+for (d=0;d<n-c-1;d++)
+{
+if (array[d]>array[d+1])
+{
+swap= array[d];
+array[d]= array[d+1];
+array[d+1]=swap;
+}
+}
+} 
+for (c=0;c<n;c++)
+printf("%d\n",array[c]);
+}
